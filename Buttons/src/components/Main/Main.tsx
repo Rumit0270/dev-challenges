a68654d032ca9a78c3bcc1a9ddd2b7f7c367d41f
Rumit Tandukar <rumit0270@gmail.com>
2020-07-16 17:17:14 +0545

    Wire up Main Component to show various button states

--- Buttons/src/components/Main/Main.tsx
+++ Buttons/src/components/Main/Main.tsx
@@ -19,6 +19,58 @@ const Main: React.FC = (): JSX.Element => {
         <ButtonDemo jsx="<Button variant='text' />">
           <Button variant="text" />
         </ButtonDemo>
+
+        <ButtonDemo jsx="<Button disableShadow />">
+          <Button disableShadow />
+        </ButtonDemo>
+
+        <div className="main-buttons--category">
+          <ButtonDemo jsx="<Button disabled />">
+            <Button disabled />
+          </ButtonDemo>
+          <ButtonDemo jsx="<Button variant='text' disabled />">
+            <Button variant="text" disabled />
+          </ButtonDemo>
+        </div>
+
+        <div className="main-buttons--category">
+          <ButtonDemo jsx="<Button startIcon='local_grocery_store' />">
+            <Button startIcon="local_grocery_store" />
+          </ButtonDemo>
+          <ButtonDemo jsx="<Button endIcon='local_grocery_store' />">
+            <Button endIcon="local_grocery_store" />
+          </ButtonDemo>
+        </div>
+
+        <div className="main-buttons--category">
+          <ButtonDemo jsx="<Button size='sm' />">
+            <Button size="sm" />
+          </ButtonDemo>
+          <ButtonDemo jsx="<Button size='md' />">
+            <Button size="md" />
+          </ButtonDemo>
+          <ButtonDemo jsx="<Button size='lg' />">
+            <Button size="lg" />
+          </ButtonDemo>
+        </div>
+
+        <div className="main-buttons--category">
+          <ButtonDemo jsx="<Button color='default' />">
+            <Button color="default" />
+          </ButtonDemo>
+
+          <ButtonDemo jsx="<Button color='primary' title='Primary' />">
+            <Button color="primary" title="Primary" />
+          </ButtonDemo>
+
+          <ButtonDemo jsx="<Button color='secondary' title='Secondary' />">
+            <Button color="secondary" title="Secondary" />
+          </ButtonDemo>
+
+          <ButtonDemo jsx="<Button color='danger' title='Danger' />">
+            <Button color="danger" title="Danger" />
+          </ButtonDemo>
+        </div>
       </div>
     </div>
   );
--- Buttons/src/components/common/Button/Button.tsx
+++ Buttons/src/components/common/Button/Button.tsx
@@ -3,10 +3,17 @@ import * as React from 'react';
 import './Button.scss';
 
 type ButtonVariant = 'default' | 'outline' | 'text';
+type ButtonSize = 'sm' | 'md' | 'lg';
+type ButtonColor = 'default' | 'primary' | 'secondary' | 'danger';
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   title?: string;
   variant?: ButtonVariant;
+  disableShadow?: boolean;
+  startIcon?: string | null;
+  endIcon?: string | null;
+  size?: ButtonSize;
+  color?: ButtonColor;
 }
 
 const buttonVariantMap = {
@@ -15,16 +22,51 @@ const buttonVariantMap = {
   text: 'text-variant',
 };
 
+const buttonSizeMap = {
+  sm: 'btn-small',
+  md: 'btn-medium',
+  lg: 'btn-large',
+};
+
+const buttonColorMap = {
+  default: '',
+  primary: 'btn-primary',
+  secondary: 'btn-secondary',
+  danger: 'btn-danger',
+};
+
 const Button: React.FC<ButtonProps> = ({
   title = 'Default',
   variant = 'default',
+  disableShadow = false,
+  startIcon = null,
+  endIcon = null,
+  size = 'md',
+  color = 'default',
   ...props
 }): JSX.Element => {
   const variantClass = buttonVariantMap[variant];
+  const shadowClass = disableShadow ? 'disable-shadow__btn' : '';
+  const titleText = props.disabled ? 'Disabled' : title;
+  const iconStart = startIcon ? (
+    <span className="material-icons btn-icon">{startIcon}</span>
+  ) : null;
+
+  const iconEnd = endIcon ? (
+    <span className="material-icons btn-icon">{endIcon}</span>
+  ) : null;
+
+  const sizeClass = buttonSizeMap[size];
+  const colorClass = buttonColorMap[color];
 
   return (
-    <button className={`${variantClass}`} {...props}>
-      {title}
+    <button
+      className={`base-button-style ${variantClass} ${shadowClass} ${sizeClass} ${colorClass}`}
+      {...props}
+    >
+      {iconStart}
+      {titleText}
+      {iconEnd}
     </button>
   );
 };
