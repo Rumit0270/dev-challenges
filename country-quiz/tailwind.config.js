00cd2550ecf35d11e740e126b32c24b167593942
Rumit Tandukar <rumit0270@gmail.com>
2020-08-01 14:47:16 +0545

    Configure tailwind for fonts and colors

--- country-quiz/tailwind.config.js
+++ country-quiz/tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    colors: {
+      light: '#F2F2F2',
+      dark: '#1D355D',
+      tropaz: '#2F527B',
+      white: '#ffffff',
+      danger: '#EA8282',
+      success: '#60BF88',
+      warn: '#F9A826',
+      primary: '#6066D0',
+    },
   },
   variants: {},
   plugins: [require('tailwindcss'), require('autoprefixer')],
