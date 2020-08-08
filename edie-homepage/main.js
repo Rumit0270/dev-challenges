48f514bd49f89626227e3e91cf40c3b083e5383a
Rumit Tandukar <rumit0270@gmail.com>
2020-08-08 17:53:22 +0545

    Add Footer and Fix layout issues

--- edie-homepage/main.js
+++ edie-homepage/main.js
@@ -1,14 +1,16 @@
-var hamburgerTogger = document.querySelector('.hamburger');
-var navbar = document.querySelector('.navbar');
-var backdrop = document.querySelector('.backdrop');
+let hamburgerTogger = document.querySelector('.hamburger');
+let navbar = document.querySelector('.navbar');
+let backdrop = document.querySelector('.backdrop');
+let navLinks = document.querySelectorAll('.nav__link');
 
+///
 function toggleNavLinks() {
   if (navbar.classList) {
     navbar.classList.toggle('navbar-expand');
   } else {
     // For IE9
-    var classes = navbar.className.split(' ');
-    var i = classes.indexOf('navbar-expand');
+    let classes = navbar.className.split(' ');
+    let i = classes.indexOf('navbar-expand');
 
     if (i >= 0) {
       classes.splice(i, 1);
@@ -20,5 +22,22 @@ function toggleNavLinks() {
   }
 }
 
+function hideMobileNavLinks() {
+  let classes = navbar.className.split(' ');
+  let i = classes.indexOf('navbar-expand');
+
+  if (i >= 0) {
+    classes.splice(i, 1);
+  }
+  navbar.className = classes.join(' ');
+}
+
 hamburgerTogger.addEventListener('click', toggleNavLinks);
 backdrop.addEventListener('click', toggleNavLinks);
+///
+
+for (let i = 0; i < navLinks.length; i++) {
+  let navLink = navLinks[i];
+
+  navLink.addEventListener('click', hideMobileNavLinks);
+}
