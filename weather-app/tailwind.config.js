1a237b5ea9a949570a5e7b876f453f0e74618bfd
Rumit Tandukar <rumit0270@gmail.com>
2020-08-11 21:55:07 +0545

    Initial boilerplate setup for Weather app

---
+++ weather-app/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      raleway: ['Raleway', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    colors: {},
+    textColor: {},
+  },
+  variants: {},
+  plugins: [],
+};
