3299003bcc6db1a536753ec39fbc46f317599b38
Rumit Tandukar <rumit0270@gmail.com>
2020-08-16 16:11:18 +0545

    Render weather forecast for next five days

---
+++ weather-app/src/components/WeatherCard.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { useRecoilValue } from 'recoil';
+
+import { IConsolidatedWeather } from '../api/weatherApiService';
+import { temperatureUnitState, TemperateUnit } from '../store/atom';
+import { getFahrenheitValue } from '../utils/temperature';
+
+interface WeatherCardProps {
+  weather: IConsolidatedWeather;
+  timestamp: string;
+}
+
+const WeatherCard: React.FC<WeatherCardProps> = ({
+  weather,
+  timestamp,
+}): JSX.Element => {
+  const temperatureUnit = useRecoilValue(temperatureUnitState);
+  let weatherImgFilename = weather.weather_state_name.replace(' ', '');
+  let maxTemp =
+    temperatureUnit === TemperateUnit.celsius
+      ? weather.max_temp
+      : getFahrenheitValue(weather.max_temp);
+  let minTemp =
+    temperatureUnit === TemperateUnit.fahrenheit
+      ? weather.min_temp
+      : getFahrenheitValue(weather.min_temp);
+
+  return (
+    <div className="flex flex-col items-center p-4 font-raleway bg-mirage flex-1 weather-card-container">
+      <span className="text-white text-center text-base font-medium leading-5">
+        {timestamp}
+      </span>
+
+      <img
+        src={require(`../assets/images/${weatherImgFilename}.png`)}
+        alt={`${weather.weather_state_name}`}
+        className=" mb-6 lg:mb-8 weather-card-image"
+      />
+
+      <div className="font-medium self-stretch flex justify-evenly">
+        <span className="text-white  text-base leading-5">
+          {Math.round(maxTemp)}
+          {temperatureUnit}
+        </span>
+        <span className="text-dark-gray2 text-base leading-5">
+          {Math.round(minTemp)}
+          {temperatureUnit}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default WeatherCard;
--- weather-app/src/layouts/WeatherInfo.tsx
+++ weather-app/src/layouts/WeatherInfo.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
-import { useRecoilState } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 
-import { TemperateUnit, temperatureUnitState } from '../store/atom';
+import {
+  TemperateUnit,
+  temperatureUnitState,
+  weatherDetailState,
+} from '../store/atom';
+import WeatherCard from '../components/WeatherCard';
+import { getFormattedDate } from '../utils/date';
 
 const WeatherInfo: React.FC = (): JSX.Element => {
+  const weatherDetail = useRecoilValue(weatherDetailState);
   const [temperatureUnit, setTemperatureUnit] = useRecoilState(
     temperatureUnitState
   );
 
-  return (
-    <section className="min-h-screen w-full lg:w-2/3 bg-black px-12 py-10 font-raleway weather-info">
+  const { loading, error, detail } = weatherDetail;
+
+  console.log(weatherDetail);
+
+  const renderTemperatureUnitButtons = () => {
+    return (
       <div className="flex justify-end">
         <button
           className={`text-lg font-bold leading-5 temperature-button ${
@@ -32,6 +43,47 @@ const WeatherInfo: React.FC = (): JSX.Element => {
           {TemperateUnit.fahrenheit}
         </button>
       </div>
+    );
+  };
+
+  const renderWeatherCards = () => {
+    if (!detail) {
+      return null;
+    }
+
+    let weatherDetailsFiltered = detail.consolidated_weather.filter(
+      (item, index) => index !== 0
+    );
+
+    return (
+      <div className="flex flex-wrap justify-between">
+        {weatherDetailsFiltered.map((item, index) => {
+          if (index === 0) {
+            return <WeatherCard weather={item} timestamp="Tomorrow" />;
+          }
+          return (
+            <WeatherCard
+              weather={item}
+              timestamp={getFormattedDate(item.applicable_date)}
+            />
+          );
+        })}
+      </div>
+    );
+  };
+
+  if (loading || error) {
+    return (
+      <section className="min-h-screen w-full lg:w-2/3 bg-black px-12 py-10 flex flex-col justify-center items-center font-raleway weather-info">
+        <span className="text-3xl text-dark-gray2">Loading...</span>
+      </section>
+    );
+  }
+
+  return (
+    <section className="min-h-screen w-full lg:w-2/3 bg-black px-12 py-10 flex flex-col justify-between font-raleway weather-info">
+      {renderTemperatureUnitButtons()}
+      {renderWeatherCards()}
     </section>
   );
 };
