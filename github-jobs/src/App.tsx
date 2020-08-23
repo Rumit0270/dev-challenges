af3a8c2144f57a815085f083bf1a1f755924d566
Rumit Tandukar <rumit0270@gmail.com>
2020-08-23 18:59:38 +0545

    Initial boilerplate setup for Github jobs

---
+++ github-jobs/src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function App() {
+  return <div>App Component</div>;
+}
+
+export default App;
---
+++ github-jobs/tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: [
+    'src/**/*.js',
+    'src/**/*.jsx',
+    'src/**/*.ts',
+    'src/**/*.tsx',
+    'public/**/*.html',
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      roboto: ['Roboto', 'sans-serif'],
+      montserrat: ['Montserrat', 'sans-serif'],
+    },
+    colors: {
+      'steel-gray': '#282538',
+      chambray: '#334680',
+      heather: '#B9BDCF',
+      heather2: '#B7BCCE',
+      'dodger-blue': '#1E86FF',
+    },
+    textColor: {
+      'steel-gray': '#282538',
+      chambray: '#334680',
+      heather: '#B9BDCF',
+      heather2: '#B7BCCE',
+      'dodger-blue': '#1E86FF',
+    },
+  },
+  variants: {},
+  plugins: [],
+};
