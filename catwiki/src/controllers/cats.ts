38697ffe5e386f9901df0b00d1f4b7b0ab72a9cc
Rumit Tandukar <rumit0270@gmail.com>
2020-09-22 23:30:19 +0545

    Add routes to fetch popular breeds

--- catwiki/src/controllers/cats.ts
+++ catwiki/src/controllers/cats.ts
@@ -1,6 +1,9 @@
 import { Request, Response, NextFunction } from 'express';
-import { searchBreedByName } from '../api/catApiService';
 
+import { searchBreedByName } from '../api/catApiService';
+import FavouriteBreed from '../models/favouriteBreed';
+
+// Search a breed by given query string
 export const getSearchBreedByName = async (
   req: Request,
   res: Response,
@@ -10,7 +13,7 @@ export const getSearchBreedByName = async (
 
   if (!searchTerm || typeof searchTerm !== 'string') {
     return res.status(400).json({
-      error: 'Invalid request',
+      error: 'Bad Request',
     });
   }
 
@@ -19,7 +22,79 @@ export const getSearchBreedByName = async (
     return res.status(200).json(response);
   } catch (err) {
     return res.status(500).json({
-      error: err,
+      error: 'Internal Server Error',
+    });
+  }
+};
+
+// Get top 10 most searched breed
+export const getPopularBreeds = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const breeds = await FavouriteBreed.find({})
+      .sort({
+        searchCount: -1,
+        name: 1,
+      })
+      .limit(10);
+
+    return res.status(200).json(breeds);
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Internal Server Error',
+    });
+  }
+};
+
+// Increment a search count of a breed when searched
+export const postSearchBreed = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const breed = req.body.breed;
+    const breedId = breed.breedId;
+
+    if (!breed || !breed.name || !breed.description || !breed.breedId) {
+      return res.status(400).json({
+        error: 'Bad Request',
+      });
+    }
+
+    const favouriteBreed = await FavouriteBreed.findOne({ breedId });
+
+    if (favouriteBreed) {
+      await FavouriteBreed.updateOne(
+        { breedId },
+        {
+          $inc: { searchCount: 1 },
+        }
+      );
+
+      return res.status(200).json({
+        message: 'Success',
+      });
+    }
+
+    const newFavouriteBreed = new FavouriteBreed({
+      name: breed.name,
+      description: breed.description,
+      breedId: breed.breedId,
+      searchCount: 1,
+    });
+
+    await newFavouriteBreed.save();
+
+    return res.status(200).json({
+      message: 'Success',
+    });
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Internal Server Error',
     });
   }
 };
--- catwiki/src/routes/cats.ts
+++ catwiki/src/routes/cats.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
-import { getSearchBreedByName } from '../controllers/cats';
+import {
+  getSearchBreedByName,
+  getPopularBreeds,
+  postSearchBreed,
+} from '../controllers/cats';
 
 const router = Router();
 
 router.get('/search', getSearchBreedByName);
+router.get('/popular', getPopularBreeds);
+router.post('/popular', postSearchBreed);
 
 export default router;
